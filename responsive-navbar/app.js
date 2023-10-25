ca4c0b5e4e9f7860986f92407acab69442842985
Hamza <jhamzaahmed@gmail.com>
2023-10-25 12:13:34 +0500

    updated responsive navbar

--- responsive-navbar/app.js
+++ responsive-navbar/app.js
@@ -1,30 +1,30 @@
 
-let width = window.screen.width;
 const midContent = document.querySelector('.mid-content');
 const social = document.querySelector('.social');
+const icon = document.querySelector('.icon')
 
-console.log(window.screen.width)
 
-if(window.screen.width < 800){
-    
-}
-
-// let mql = window.matchMedia("(max-width: 600px)");
-
-// document.querySelector(".mq-value").innerText = mql.matches;
-
-const mql = window.matchMedia("(max-width: 600px)");
-
-mql.onchange = (e) => {
-  if (e.matches) {
+window.onresize = function (){
+  let width = window.innerWidth;
+  
+  if(width < 700){
     midContent.classList.add('invisible');
     social.classList.add('invisible');
-    /* the viewport is 600 pixels wide or less */
-    console.log("This is a narrow screen — less than 600px wide.");
-} else {
+
+    icon.classList.add('visible');
+    icon.classList.remove('invisible');
+    
+    midContent.classList.remove('visible');
+    social.classList.remove('visible');
+  }
+  else {
     midContent.classList.add('visible');
     social.classList.add('visible');
-    /* the viewport is more than 600 pixels wide */
-    console.log("This is a wide screen — more than 600px wide.");
+    
+    icon.classList.add('invisible');
+    icon.classList.remove('visible');
+
+    midContent.classList.remove('invisible');
+    social.classList.remove('invisible');
   }
-};
+}
