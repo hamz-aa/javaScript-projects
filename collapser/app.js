c93d7c3b6f61dc2aa4f251263e34e75f6d406f6f
Hamza <jhamzaahmed@gmail.com>
2023-10-27 15:20:23 +0500

    completed html and css for collapser

---
+++ collapser/app.js
@@ -0,0 +1,9 @@
+
+const subContent = document.querySelectorAll('.subcontent');
+const subContent1 = document.querySelector('.subcontent-1');
+const subContent2 = document.querySelector('.subcontent-2');
+const subContent3 = document.querySelector('.subcontent-3');
+
+function collapse(){
+    
+}
