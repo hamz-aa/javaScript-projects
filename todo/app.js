df21129794f66310e6fc1011f326a6ce4426e954
Hamza <jhamzaahmed@gmail.com>
2023-11-02 16:33:03 +0500

    updated todo.js

--- todo/app.js
+++ todo/app.js
@@ -1,27 +1,55 @@
-const listItemInput = document.querySelector('#listItemInput');
-const myUnorderdList = document.querySelector('ul');
+const btn = document.querySelector('.btn');
+const todoData = document.querySelector('.todo-data');
 
-function submitHandler() {
-    let val = listItemInput.value;
-    const li = `<li class="${val}">
-    ${val}
-    <div>
-        <span class="listButton" id="edit" onclick="editHandler('${val}')">Edit</span>
-        <span class="listButton" id="delete" onclick="deleteHandler('${val}')">Delete</span>
-    </div>
-    </li>`
+let count = 0;
 
-    myUnorderdList.innerHTML += li;
-    val = '';
+function submitHandler(){
+    count++;
+
+    const inp = document.querySelector('.input');
+    const val = inp.value;
+
+    const li = document.createElement('li');
+    const p = document.createElement('p');
+    const editBtn = document.createElement('button');
+    const deleteBtn = document.createElement('button');
+
+    p.textContent = val;
+    editBtn.textContent = 'Edit';
+    deleteBtn.textContent = 'Delete';
+
+    editBtn.setAttribute('class', 'editBtn');
+    deleteBtn.setAttribute('class', 'deleteBtn');
+
+    editBtn.setAttribute('onclick', `editHandler(${count})`);
+    deleteBtn.setAttribute('onclick', `deleteHandler(${count})`);
+
+    li.setAttribute('class', `todo-data-wrapper todo-data-wrapper-${count}`);
+    p.setAttribute('class', 'todo-text');
+
+    li.append(p);
+    li.append(editBtn);
+    li.append(deleteBtn);
+
+    todoData.prepend(li);
+    inp.value = '';
 }
 
-function editHandler(val){
-    const li = document.querySelector(`.${val}`);
-    listItemInput.value = li.children[0].textContent;
+function editHandler(num){
+    const inp = document.createElement('input');
+    inp.setAttribute('type', 'text');
+    inp.setAttribute('class', 'editInp');
+
+    const li = document.querySelector(`.todo-data-wrapper-${num}`);
+    const p = document.querySelector(`.todo-data-wrapper-${num} p`);
+
+    inp.value = p.textContent;
+    p.remove();
+    li.prepend(inp);
 }
 
-function deleteHandler(val){
-    const li = document.querySelector(`.${val}`);
+function deleteHandler(num){
+    const li = document.querySelector(`.todo-data-wrapper-${num}`);
+    li.innerHTML = ''
     li.remove();
-    li.innerHTML = '';
 }
