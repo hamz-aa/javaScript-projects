763d22687e55c420d183fc73090cd321cebd10f9
Hamza <jhamzaahmed@gmail.com>
2024-01-02 09:53:26 +0500

    updated menu

---
+++ food-menu-api/app.js
@@ -0,0 +1,7 @@
+// const url = `https://forkify-api.herokuapp.com/api/v2/recipes?search=pizza`;
+
+// fetch(url)
+//   .then((response) => response.json())
+//   .then((json) => json.data)
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
