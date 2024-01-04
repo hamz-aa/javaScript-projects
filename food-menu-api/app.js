03509ecb61434165411af1c77d5f701a0c1885b8
Hamza <jhamzaahmed@gmail.com>
2024-01-04 18:50:00 +0500

    started working on javascript

--- food-menu-api/app.js
+++ food-menu-api/app.js
@@ -1,7 +1,24 @@
-// const url = `https://forkify-api.herokuapp.com/api/v2/recipes?search=pizza`;
+const searchValue = document.querySelector("#search-box").value;
+const searchBtn = document.querySelector("#search-btn");
+const leftTemplate = document.querySelector("#left-sec-template");
+const leftSection = document.querySelector(".left-section");
 
-// fetch(url)
-//   .then((response) => response.json())
-//   .then((json) => json.data)
-//   .then((data) => console.log(data))
-//   .catch((err) => console.log(err));
+searchBtn.addEventListener("click", () => {
+  const url = `https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchValue}`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((json) => json.data)
+    .then((data) => data.recipes)
+    .then((recipes) => displaySearches(recipes))
+    .catch((err) => console.log(err));
+});
+
+function displaySearches(recipes) {
+  for (let i = 0; i < 10; i++) {
+    let cloneNode = leftTemplate.content.cloneNode(true);
+    let img = cloneNode.querySelector("img");
+    let foodTitle = cloneNode.querySelector(".food-title");
+    let foodDescription = cloneNode.querySelector(".food-description");
+  }
+}
